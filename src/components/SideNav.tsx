3805dac36d306dfaf9aed3d382ee7580617b8710
Abhinand Arun <abhinandarun2015@gmail.com>
2023-09-15 23:59:18 +0530

    Modified Navigation elements

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -5,13 +5,15 @@ import { usePathname } from 'next/navigation'
 import { Roboto } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
 import {
   CalendarDays,
   CreditCard,
+  HeartPulse,
   Home,
   MessageSquare,
   Settings,
-  Stethoscope,
 } from 'lucide-react'
 
 const inter = Roboto({ weight: '500', subsets: ['latin'] })
@@ -21,37 +23,37 @@ const routes = [
     label: 'Home',
     icon: Home,
     href: '/',
-    color: 'text-sky-500',
+    disabled: false,
   },
   {
     label: 'Doctors',
-    icon: Stethoscope,
+    icon: HeartPulse,
     href: '/doctors',
-    color: 'text-sky-500',
+    disabled: true,
   },
   {
     label: 'Appointments',
     icon: CalendarDays,
     href: '/appointments',
-    color: 'text-sky-500',
+    disabled: true,
   },
   {
     label: 'Messages',
     icon: MessageSquare,
     href: '/chat',
-    color: 'text-sky-500',
+    disabled: true,
   },
   {
     label: 'Payments',
     icon: CreditCard,
     href: '/payments',
-    color: 'text-sky-500',
+    disabled: true,
   },
   {
     label: 'Settings',
     icon: Settings,
     href: '/settings',
-    color: 'text-sky-500',
+    disabled: true,
   },
 ]
 
@@ -61,27 +63,34 @@ export const SideNav = () => {
   return (
     <div className="links mt-16 mx-4 flex flex-col gap-2 items-center">
       {routes.map((route) => (
-        <Link
+        <Button
           key={route.href}
-          href={route.href}
-          className={cn(
-            'h-10 w-full p-3 mx-4 flex justify-start items-center cursor-pointer rounded-lg text-sm font-medium hover:bg-background',
-            pathname === route.href
-              ? 'text-blue-500 bg-background'
-              : 'text-zinc-500',
-            inter.className
-          )}
+          variant={'ghost'}
+          disabled
+          className="w-full justify-start"
+          asChild
         >
-          <div className="flex">
-            <route.icon
-              className={cn(
-                'h-5 w-5 mr-3',
-                pathname === route.href ? 'text-blue-500' : 'text-zinc-500'
-              )}
-            />
-            {route.label}
-          </div>
-        </Link>
+          <Link
+            href={route.href}
+            className={cn(
+              'h-10 w-full p-3 mx-4 flex justify-start items-center cursor-pointer rounded-lg text-sm font-medium hover:bg-background',
+              pathname === route.href
+                ? 'text-blue-500 bg-background'
+                : 'text-zinc-500',
+              inter.className
+            )}
+          >
+            <div className="flex ">
+              <route.icon
+                className={cn(
+                  'h-5 w-5 mr-3',
+                  pathname === route.href ? 'text-blue-500' : 'text-zinc-500'
+                )}
+              />
+              {route.label}
+            </div>
+          </Link>
+        </Button>
       ))}
     </div>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,34 @@
+import { Search, Bell, Menu } from 'lucide-react'
+
+import { Separator } from '@/components/ui/separator'
 import { Input } from '@/components/ui/input'
-import { Search, Bell } from 'lucide-react'
-import { Separator } from './ui/separator'
+import { Button } from '@/components/ui/button'
+
 import { UserAvatar } from './UserAvatar'
 import { SelectDemo } from './UserDropdown'
 
 export const NavBar = () => {
   return (
-    <nav className="mx-8 my-4">
+    <nav className="px-4 md:px-8 py-3 md:py-4">
       <div className="relative flex items-center justify-between gap-4">
-        <Input
-          type="text"
-          placeholder="Search by doctor's name/speciality etc"
-          className="pl-10 max-w-[450px]"
-        />
-        <Search className="absolute top-2 left-2 h-5 w-5 " />
+        <div className="flex items-center gap-5">
+          <Button variant={'ghost'} size={'icon'} className="w-6 h-6 md:hidden">
+            <Menu />
+          </Button>
+          <Input
+            type="text"
+            placeholder="Search..."
+            className="pl-10 max-w-[450px]"
+          />
+          <Search className="absolute top-3 left-12 md:left-2 h-5 w-5 " />
+        </div>
         <div className="relative flex items-center gap-4 h-[45px] pr-8">
           <Bell className="h-6 w-6 cursor-pointer" />
-          <Separator orientation="vertical" decorative />
+          <Separator
+            orientation="vertical"
+            decorative
+            className="w-[1px] bg-zinc-400"
+          />
           <UserAvatar />
           <SelectDemo />
         </div>
